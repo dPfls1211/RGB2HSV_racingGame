dd602fe09428f2ce743d601215e40f9b3408a7bb
dPfls1211 <dPfls1211@gmail.com>
2022-08-13 03:26:49 +0900

    RGB2HSV

---
+++ Assets/MarkDestroy.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarkDestroy : MonoBehaviour {
+
+    public float DeletesPoint = 0;
+    // Update is called once per frame
+    void Update()
+    {
+        DeletesPoint += Time.deltaTime;
+        if (DeletesPoint > 1)
+            Destroy(gameObject);
+    }
+}
---
+++ Assets/PlayerMove.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+
+    public float speed = 7.0f;
+	// Use this for initialization
+	void Start () {
+        Debug.Log("START");
+    }
+    
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            Debug.Log("A");
+            //transform.position += new Vector3(-speed, 0, 0) * Time.deltaTime;
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.S))
+            transform.Translate(Vector3.back * speed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.W))
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+
+
+    }
+}
